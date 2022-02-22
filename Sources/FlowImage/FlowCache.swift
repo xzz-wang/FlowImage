4bab20cb47a2aa460b7e3ade46f3421f11fb50ae
Xuezheng Wang <xzzwang@icloud.com>
2022-02-22 15:11:44 -0800

    we are actually including the cache this time.

---
+++ Sources/FlowImage/FlowCache.swift
@@ -0,0 +1,45 @@
+//
+//  FlowCache.swift
+//  ClassHouse
+//
+//  Created by Xuezheng Wang on 2/5/22.
+//
+
+import Foundation
+import UIKit
+
+///
+/// Used in Picture's getUIImageFromCache() to produce your image.
+///
+actor FlowCache {
+    static let instance = FlowCache()
+
+    /// Maybe in the future we can add an expire time to this.
+    private struct CacheEntry {
+        let image: FlowImage
+    }
+
+    private var tasks: [FlowImage.ID: Task<CacheEntry, Error>] = [:]
+
+    /// Cache the image or replace the cached image.
+    func cache(_ picture: FlowImage) {
+        let newTask = Task { () -> CacheEntry in
+            do {
+                let preparedPic = try await picture.prepareForDisplay()
+                return CacheEntry(image: preparedPic)
+            } catch {
+                tasks[picture.id] = nil
+                throw error
+            }
+        }
+        tasks[picture.id]?.cancel()
+        tasks[picture.id] = newTask
+    }
+
+    func get(_ picture: FlowImage, forceReCache: Bool = false) async throws -> UIImage {
+        if forceReCache || tasks[picture.id] == nil {
+            cache(picture)
+        }
+        return try await tasks[picture.id]!.value.image.getUIImage()
+    }
+}
