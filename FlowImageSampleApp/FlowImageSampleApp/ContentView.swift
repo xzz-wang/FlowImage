20c6e761b852a62a95cdeb651221950f3b7d50c2
Xuezheng Wang <xzzwang@icloud.com>
2022-03-06 00:52:36 -0800

    Better example in sample app.

--- FlowImageSampleApp/FlowImageSampleApp/ContentView.swift
+++ FlowImageSampleApp/FlowImageSampleApp/ContentView.swift
@@ -6,42 +6,14 @@
 //
 
 import SwiftUI
-import FlowImage
 
 struct ContentView: View {
-    @State var image = URLFlowImage(URL(string: "https://i.guim.co.uk/img/media/b563ac5db4b4a4e1197c586bbca3edebca9173cd/0_12_3307_1985/master/3307.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=61a26bf43da26e4ca97e932e5ee113f7")!)
+    @State var image = sampleImages[0]
     var body: some View {
-        VStack {
-            ImageView(image: image)
-            ImageView(image: image)
-            ImageView(image: image)
-
-            Button {
-                image = URLFlowImage(URL(string: "https://img1.baidu.com/it/u=1667044935,2962646688&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=708")!)
-            } label: {
-                Text("Change")
-            }
-        }
-    }
-
-    private struct ImageView: View {
-        let image: FlowImage
-
-        var body: some View {
-            FlowImageView(image: image) { image, state in
-                VStack {
-                    Text("Yayy")
-                    if state == .displaying, let image = image {
-                        Image(uiImage: image)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                    } else if state == .error {
-                        Text("There was an error!")
-                    } else if state == .loading {
-                        Text("Loading!")
-                    } else {
-                        Text("No image")
-                    }
+        NavigationView {
+            VStack {
+                NavigationLink("Level 1: Using URLFlowImage") {
+                    Level1View()
                 }
             }
         }
---
+++ FlowImageSampleApp/FlowImageSampleApp/Level1View.swift
@@ -0,0 +1,78 @@
+//
+//  Level1View.swift
+//  FlowImageSampleApp
+//
+//  Created by Xuezheng Wang on 3/5/22.
+//
+
+import FlowImage
+import SwiftUI
+
+struct Level1View: View {
+    @State var image: FlowImage = sampleImages[0]
+    
+    var body: some View {
+        VStack {
+            SampleImageView(image: image)
+                .frame(height: 400)
+
+            ForEach(sampleImages.indices, id: \.self) { idx in
+                let sampleImage = sampleImages[idx]
+                Button {
+                    image = sampleImage
+                } label: {
+                    Text("Image #\(idx)")
+                }
+            }
+        }
+    }
+}
+
+struct SampleImageView: View {
+    let image: FlowImage
+
+    var body: some View {
+        FlowImageView(image: image) { image, state in
+            ZStack {
+                if state == .displaying {
+                    if let image = image {
+                        // Being in here means we have a proper image to display
+                        Image(uiImage: image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+
+                    } else {
+                        // If the image is nil and we are displaying, it means the
+                        // FlowImage that's passed in is nil.
+                        Text("No Image")
+                    }
+                } else if state == .error {
+                    // If state == .error, it means an error occurred, and image
+                    // value is set to nil.
+                    Text("There was an error!")
+
+                } else if state == .loading {
+                    if let image = image {
+                        // If image is not nil when loading, it will be the image
+                        // before we started loading.
+                        Image(uiImage: image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .colorMultiply(.init(red: 0.7, green: 0.7, blue: 0.7))
+
+                    }
+
+                    // We overlay a progress view if we are loading.
+                    ProgressView()
+                        .progressViewStyle(.circular)
+                }
+            }
+        }
+    }
+}
+
+struct Level1View_Previews: PreviewProvider {
+    static var previews: some View {
+        Level1View()
+    }
+}
